e49832d4485f96a42dbd59854475252f1c96fdca
vitaliihapon <v.gapon@smileexpo.com.ua>
2025-01-25 20:45:41 -0700

    Remove spaces

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -36,20 +36,16 @@ class SecurityController extends AbstractController
     {
 
         $secret = 'crypto_deploy'; // Replace with your GitHub webhook secret
-
         // Verify the request signature
         $signature = 'sha256=' . hash_hmac('sha256', file_get_contents('php://input'), $secret);
         if (!hash_equals($signature, $_SERVER['HTTP_X_HUB_SIGNATURE_256'])) {
             http_response_code(403);
             exit('Invalid signature');
         }
-
         // Execute Git pull
         $output = [];
         exec('cd /home/u538818725/domains/growupcrypto.site/public_html && git pull 2>&1', $output);
         echo implode("\n", $output);
-
-
         //return new Response($message);
     }
 }
