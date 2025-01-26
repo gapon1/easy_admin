f687a0b1b6953701e6c4ad263699fad7e0983efb
vitaliihapon <v.gapon@smileexpo.com.ua>
2025-01-25 21:12:19 -0700

    Delete Deploy function

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -31,32 +31,4 @@ class SecurityController extends AbstractController
     {
         throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
-
-    #[Route('/deploy', name: 'app_deploy')]
-    public function deploy(Request $request): Response
-    {
-        // Test deployment
-        $headers = getallheaders();
-        file_put_contents(dirname(__DIR__, 2).'/var/log/headers.log', print_r($headers, true));
-
-        $secret = 'crypto_deploy'; // Replace with your GitHub webhook secret
-
-        $signatureHeader = $headers['X-Hub-Signature-256'] ?? null; // Fetch the header properly
-
-        if ($signatureHeader === null) {
-            return new Response('Missing X-Hub-Signature-256 header', 400);
-        }
-
-        $payload = $request->getContent();
-        $signature = 'sha256=' . hash_hmac('sha256', $payload, $secret);
-
-        if (!hash_equals($signature, $signatureHeader)) {
-            return new Response('Invalid signature', 403);
-        }
-
-        $output = [];
-        exec('cd /home/u538818725/domains/growupcrypto.site/public_html && git pull 2>&1', $output);
-
-        return new Response(implode("\n", $output), 200);
-    }
 }
